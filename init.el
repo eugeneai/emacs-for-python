0c78eda562d58af72c737c9e81aac3900c2da8ae
Evgeny Cherkashin <eugeneai@irnok.net>
2016-04-29 17:02:48 +0800

    Added Rust mode compiler.

--- init.el
+++ init.el
@@ -29,6 +29,17 @@
 
 (setq magit-last-seen-setup-instructions "1.4.0")
 
+(defun lcl:rust-compile-hook ()
+  (require 'compile)
+  (set (make-local-variable 'compile-command)
+       (if (locate-dominating-file (buffer-file-name) "Cargo.toml")
+           "cargo run"
+         (format "rustc %s && %s" (buffer-file-name)
+                 (file-name-sans-extension (buffer-file-name))))))
+
+(setq-default compilation-read-command nil)
+(add-hook 'rust-mode-hook 'lcl:rust-compile-hook)
+
 (require 'auctex-latexmk)
 (auctex-latexmk-setup)
 
